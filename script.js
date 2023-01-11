77b84b7dcb66aeaf16c45eebad9756fe1b3f9731
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-01-11 12:05:20 +0000

    Add DOM variables

---
+++ script.js
@@ -0,0 +1,12 @@
+// DOM variables
+const playerChoice = document.querySelector('.playerChoice');
+const computerChoice = document.querySelector('.computerChoice');
+const resultsText = document.querySelector('.resultsText');
+const selectionButtons = document.querySelectorAll('.selectionButton');
+const playAgainButton = document.querySelector('.playAgain')
+const playerScoreSpan = document.querySelector('[data-player-score]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+const finalWinner = document.querySelector('.finalWinner');
+const rockButton = document.querySelector('.rockButton');
+const paperButton = document.querySelector('.paperButton');
+const scissorsButton = document.querySelector('.scissorsButton');
